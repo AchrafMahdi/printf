92692bb5fe2d186cc2592e1857dbef2b0baee81c
Achraf <achraf@example.com>
2024-08-01 05:12:27 +0100

    nope betty was not fixed...

--- _printf.c
+++ _printf.c
@@ -15,7 +15,6 @@ unsigned int i = 0;
 int num = 0;
 if (format == NULL)
 return (-1);
-
 va_start(args, format);
 while (format[i] != '\0')
 {
@@ -24,18 +23,35 @@ if (format[i] == '%')
 i++;
 switch (format[i])
 {
-case 'c': _putchar(va_arg(args, int)); num++; break;
-case 's': num += printString(args); break;
-case '%': _putchar('%'); num++; break;
+case 'c':
+_putchar(va_arg(args, int));
+num++;
+break;
+case 's':
+num += printString(args);
+break;
+case '%':
+_putchar('%');
+num++;
+break;
 case 'd':
 case 'i':
 {
 int n = va_arg(args, int);
-if (n < 0) { _putchar('-'); n = -n; num++; }
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+num++;
+}
 num += printNumber(n);
 break;
 }
-default: _putchar('%'); _putchar(format[i]); num += 2; break;
+default:
+_putchar('%');
+_putchar(format[i]);
+num += 2;
+break;
 }
 }
 else
