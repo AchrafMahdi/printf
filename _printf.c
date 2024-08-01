9248d7702efeb35efa89d7440c650d58203463c4
Achraf <achraf@example.com>
2024-08-01 05:08:33 +0100

    betty fixed

--- _printf.c
+++ _printf.c
@@ -15,6 +15,7 @@ unsigned int i = 0;
 int num = 0;
 if (format == NULL)
 return (-1);
+
 va_start(args, format);
 while (format[i] != '\0')
 {
@@ -23,35 +24,18 @@ if (format[i] == '%')
 i++;
 switch (format[i])
 {
-case 'c':
-_putchar(va_arg(args, int));
-num++;
-break;
-case 's':
-num += printString(args);
-break;
-case '%':
-_putchar('%');
-num++;
-break;
+case 'c': _putchar(va_arg(args, int)); num++; break;
+case 's': num += printString(args); break;
+case '%': _putchar('%'); num++; break;
 case 'd':
 case 'i':
 {
 int n = va_arg(args, int);
-if (n < 0)
-{
-_putchar('-');
-n = -n;
-num++;
-}
+if (n < 0) { _putchar('-'); n = -n; num++; }
 num += printNumber(n);
 break;
 }
-default:
-_putchar('%');
-_putchar(format[i]);
-num += 2;
-break;
+default: _putchar('%'); _putchar(format[i]); num += 2; break;
 }
 }
 else
